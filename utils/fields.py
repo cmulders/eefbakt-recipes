5ab6d84213490894ebc8cc8347cf29a20efa4722
cmulders <coen.mulders@gmail.com>
2020-08-02 21:59:54 +0200

    call super

--- utils/fields.py
+++ utils/fields.py
@@ -3,20 +3,18 @@ from django.core.exceptions import ImproperlyConfigured
 
 
 class CreatingModelChoiceField(forms.ModelChoiceField):
-    def __init__(self, queryset, *args, creation_field=None, **kwargs):
-        opts = queryset.model._meta
+    def __init__(self, *args, **kwargs):
+        self.creation_field = kwargs.pop("creation_field")
+
+        super().__init__(*args, **kwargs)
+
+        opts = self.queryset.model._meta
         model_field_names = [f.name for f in opts.get_fields()]
 
-        if not creation_field:
+        if not self.creation_field in model_field_names:
             raise ImproperlyConfigured(
-                f"'creation_field' should not be empty, but one of {', '.join(model_field_names)}"
+                f"'{self.creation_field}' should be one of {','.join(model_field_names)}"
             )
-        if not creation_field in model_field_names:
-            raise ImproperlyConfigured(
-                f"'{creation_field}' should be one of {','.join(model_field_names)}"
-            )
-
-        self.creation_field = creation_field
 
     def to_python(self, value):
         try:
