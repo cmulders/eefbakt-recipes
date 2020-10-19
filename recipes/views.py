9e45ac0b957e64859216096b5cfb8097dffadd07
cmulders <coen.mulders@gmail.com>
2020-10-19 22:42:32 +0200

    Update homepage with better styling and layout

--- recipes/views.py
+++ recipes/views.py
@@ -6,13 +6,13 @@ class IndexView(generic.TemplateView):
     template_name = "home.html"
 
     def get_last_products(self):
-        return Product.objects.order_by("-updated_at").all()[:5]
+        return Product.objects.order_by("-updated_at").all()
 
     def get_last_recipes(self):
-        return Recipe.objects.order_by("-updated_at").all()[:5]
+        return Recipe.objects.order_by("-updated_at").all()
 
     def get_last_sessions(self):
-        return Session.objects.order_by("-updated_at").all()[:5]
+        return Session.objects.order_by("-updated_at").all()
 
     def get_context_data(self, **kwargs):
         kwargs.update(
