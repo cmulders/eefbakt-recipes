2b581ee55b3c212b50d68012d2c10ac7dfc636aa
cmulders <coen.mulders@gmail.com>
2020-10-25 20:24:16 +0100

    Show blurred image for missing images

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from data.models import Product, Recipe, Session
+from data.models import ImageTag, Product, Recipe, Session
 from django.views import generic
 
 
@@ -14,12 +14,16 @@ class IndexView(generic.TemplateView):
     def get_last_sessions(self):
         return Session.objects.order_by("-updated_at").all()
 
+    def get_random_images(self):
+        return ImageTag.objects.order_by("?")
+
     def get_context_data(self, **kwargs):
         kwargs.update(
             {
                 "last_products": self.get_last_products(),
                 "last_recipes": self.get_last_recipes(),
                 "last_sessions": self.get_last_sessions(),
+                "random_images": self.get_random_images(),
             }
         )
 
