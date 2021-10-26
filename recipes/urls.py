17a4d0f00eb37d67f716d282e8a3b50e77aa9489
cmulders <coen.mulders@gmail.com>
2021-10-26 22:45:16 +0200

    Initial apps for Sweet & Savory

--- recipes/urls.py
+++ recipes/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path("home/", IndexView.as_view(), name="home"),
     path("admin/", admin.site.urls),
     path("data/", include("data.urls")),
+    path("savory/", include("savory.urls")),
+    path("sweet/", include("sweet.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
